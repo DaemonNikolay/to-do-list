cb21470999e1849ebb67ba0c546a145b8e2b8a0b
Nikolay Eckert <nikolay-eckert@yandex.ru>
2019-10-16 00:12:01 +0700

    Init; create mocup of screen

---
+++ to-do-list/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  to-do-list
+//
+//  Created by Nikolay Eckert on 15.10.2019.
+//  Copyright © 2019 Nikolay Eckert. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
